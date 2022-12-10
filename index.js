adc5c7cbe7df28db3c27d01faa9e6837c16f9ce3
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 15:26:32 -0500

    CM

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-ReactDOM.render(
-<ul><li>One</li><li>Two</li></ul>,
+// This is an example of a simple functional component created and then rendered using a self-closing tag and the render method
+const MainContent = () => {
+    return (
+        <div>
+            <h1>This is a Simple Functional Component</h1>
+            <h1>React can include these types of components</h1>
+            <ul>
+                <li>JSX Elements</li>
+                <li>Simple Functional Components</li>
+                <li>ES6 Stateless Components</li>
+                <li>React Stateful Components</li>
+            </ul>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<MainContent/>,
  document.getElementById('root')
  )
 
