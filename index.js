909ea20e7de48699abd1bf41e8a88eb92a928986
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 15:35:37 -0500

    Cm

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 // This is an example of a simple functional component created and then rendered using a self-closing tag and the render method
 const MainContent = () => {
-    return (
-        <div>
-            <h1>This is a Simple Functional Component</h1>
-            <h1>React can include these types of components</h1>
-            <ul>
-                <li>JSX Elements</li>
-                <li>Simple Functional Components</li>
-                <li>ES6 Stateless Components</li>
-                <li>React Stateful Components</li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>This is a Simple Functional Component</h1>
+      <h1>React can include these types of components</h1>
+      <ul>
+        <li>JSX Elements</li>
+        <li>Simple Functional Components</li>
+        <li>ES6 Stateless Components</li>
+        <li>React Stateful Components</li>
+      </ul>
+    </div>
+  );
+};
 
+const SimpleComponent = () => {
+  return (
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+      </tr>
+      <tr>
+        <td>Franklin</td>
+        <td>Miranda</td>
+      </tr>
+      <tr>
+        <td>Anna</td>
+        <td>Miranda</td>
+      </tr>
+    </table>
+  );
+};
 
-
-ReactDOM.render(<MainContent/>,
- document.getElementById('root')
- )
-
+ReactDOM.render(
+  <div>
+    <MainContent />
+    <SimpleComponent />
+  </div>,
+  document.getElementById('root')
+);
