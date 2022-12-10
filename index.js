aeec6f5abdbe3ea65ac486bb5a9376d32b8ffa15
FranklinMiranda <fdm61344@gmail.com>
2022-12-10 15:46:04 -0500

    Cm

--- index.js
+++ index.js
@@ -40,3 +40,10 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+
+// Imperative Example of creating a h1 tag on the webpage
+ const h1 = document.createElement('h1')
+ h1.textContent = "This is an imperative way to program"
+ h1.className = 'header'
+ document.getElementById('root').append(h1)
